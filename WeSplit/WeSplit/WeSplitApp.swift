4a60e0c357ac249debbc067b6664ac42c2cf1042
Pedro Gabriel <pedro.torres@usemobile.xyz>
2021-09-02 10:29:49 -0300

    ⭐️: WeSplit lesson done

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Usemobile on 02/09/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
